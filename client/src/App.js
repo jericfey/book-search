6f95f1b903ea81abcf8cbfa22d095e03bda933e3
Jeri Fey <jericfey@gmail.com>
2021-03-06 20:51:04 -0700

    Added routes to final pages in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Books from "./components/Books/Books";
+import SavedBooks from "./pages/SavedBooks";
+import SearchBooks from "./pages/SearchBooks";
+import notFound from "./pages/notFound";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route exact path="/saved/:id" component={SavedBooks} />
+          <Route component={notFound} />
+        </Switch>
+        <Books />
+      </div>
+    </Router>
   );
 }
 

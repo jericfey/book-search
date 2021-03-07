572a1dc7159e5828bb25fba4182df065b255bd20
Jeri Fey <jericfey@gmail.com>
2021-03-06 20:56:53 -0700

    Updated API with missing route for calling book by ID

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,15 +2,23 @@ import axios from "axios";
 const BASEURL = "https://www.googleapis.com/books/v1/volumes";
 
 export default {
-  search: function (query) {
+  //search for books from Google Book API using query string
+  googleSearch: function (query) {
     return axios.get(BASEURL + query);
   },
+  //Get all books from DB
   getBooks: function () {
     return axios.get("/api/books");
   },
+  //Get a book with specific ID from DB
+  getBook: function () {
+    return axios.get(`/api/books/${id}`);
+  },
+  //Save book to DB
   saveBook: function (savedBook) {
     return axios.post("/api/books", savedBook);
   },
+  //Delete book from DB based on ID
   deleteBook: function (id) {
     return axios.delete(`/api/books/${id}`);
   },

8c5d52eec317a3487b5bc9236a1af6600e066742
Jeri Fey <jericfey@gmail.com>
2021-03-07 19:54:44 -0700

    removed duplicate db connections in server.js

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 
 // Define API routes here
 // Add routes, both API and view
 app.use(routes);
 
-//Connet to mongoose
+//Connet to Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
 });
